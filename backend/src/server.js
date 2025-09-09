1caeac9a83f2bff49e4c2b05d1641066a2186cfb
NQAnh01 <ngquocanh280101@gmail.com>
2025-09-09 21:26:17 +0700

    setup deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,19 +3,33 @@ import taskRoutes from './routes/taskRouters.js'
 import { connectDB } from './config/db.js';
 import dotenv from "dotenv";
 import cors from 'cors';
+import path from 'path';
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+const __dirname = path.resolve();
+
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-  origin: 'http://localhost:5173', // Adjust this to your frontend URL
-}));
+if (process.env.NODE_ENV !== 'production') {
+  app.use(cors({
+    origin: 'http://localhost:5173', // Adjust this to your frontend URL
+  }));
+}
 
 app.use("/api/tasks", taskRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  // fallback cho Vue/React router
+  app.use((req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 connectDB().then(() => {
   console.log('Database connected');
